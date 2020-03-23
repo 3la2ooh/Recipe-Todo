73725569c6a695bc475bd672d80cf6cad1c5fe3d
Alaa Alkhater <jtrgmd@outlook.com>
2020-03-23 22:52:34 +0300

    Fix the file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './header/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './header/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './header/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './header/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { RecipeDetailComponent } from './header/recipe-detail/recipe-detail.comp
     ShoppingListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
