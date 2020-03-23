380949eae4ffc50ae90b40f2de9fd30eb74434b8
Alaa Alkhater <jtrgmd@outlook.com>
2020-03-24 00:26:07 +0300

    Finish phase 1 of project. Next is 1) data communication between components 2) separating features in seperate pages

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simple a test.", "https://upload.wikimedia.org/wikipedia/commons/8/81/Nigella_Lawson%E2%80%99s_red_kidney_bean_dip.jpg")
+  ];
 
   constructor() { }
 
