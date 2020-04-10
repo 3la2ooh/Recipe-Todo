06039d7676229437846628ea159e234360e6ff9b
Alaa Alkhater <jtrgmd@outlook.com>
2020-04-11 00:19:32 +0300

    Add ability to add an item to the shopping list

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent implements OnInit, AfterContentInit {
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
+
+  @Output() newIngredient = new EventEmitter<{name: string, amount: number}>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    
+  }
+
+  onAddIngredient() {
+    const ingredientName = this.nameInputRef.nativeElement.value;
+    const ingredientAmount = this.amountInputRef.nativeElement.value;
+    
+    this.newIngredient.emit({
+      name: ingredientName,
+      amount: ingredientAmount
+    });
   }
 
 }
