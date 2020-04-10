60ed82ef88f0084cabd4d2a658a89d4785ec4c1b
Alaa Alkhater <jtrgmd@outlook.com>
2020-04-10 22:11:41 +0300

    Add Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedPage = 'recipes';
+
+  onNavigate(clickedName: string) {
+    this.loadedPage = clickedName;
+  }
 }
